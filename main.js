9014c5f60f2041643ebe914d7c3c568d655f7f6f
LucasBorenstein <borensteinlucas@gmail.com>
2023-10-19 19:42:28 -0300

    listo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// elementos del dom
 const inputElement = document.getElementById('input1');
 const buttonElement = document.getElementById('button1');
 const outputElement = document.getElementById('output');
@@ -7,24 +7,24 @@ const outputElement = document.getElementById('output');
 buttonElement.addEventListener('click', () => {
     const inputData = inputElement.value;
 
-   
+    // valida que se haya ingresado algo
     if (inputData.trim() === '') {
         alert('Por favor, ingrese algo válido.');
         return;
     }
 
-
+    // realiza algun procesamiento con los datos de entrada
     const processedData = processData(inputData);
 
-    
+    // modificar el dom para mostrar resultados
     outputElement.innerHTML = `Resultado: ${processedData}`;
 
-    
+    // almacenar datos en el Storage en formato JSON
     const dataToStore = { input: inputData, result: processedData };
     localStorage.setItem('data', JSON.stringify(dataToStore));
 });
 
-
+// recuperar datos del Storage al cargar la página
 window.addEventListener('load', () => {
     const storedData = localStorage.getItem('data');
     if (storedData) {
@@ -34,8 +34,9 @@ window.addEventListener('load', () => {
     }
 });
 
-
+// función para procesar los datos de entrada y que salga en mayuscula
 function processData(input) {
     
     return input.toUpperCase(); 
 }
+
